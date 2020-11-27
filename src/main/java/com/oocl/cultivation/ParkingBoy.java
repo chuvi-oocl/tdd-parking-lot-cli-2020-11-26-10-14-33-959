058b38abd45669b9a1b7677ffe115c6c4bbd6bce
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-27 15:05:00 +0800

    [Parking][Victor] feat: Complete parking boy call parking lot park function test

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,9 +1,13 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    private ParkingLot parkingLot;
+
     public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
     }
 
     public void park(Car car) {
+        this.parkingLot.park(car);
     }
 }
