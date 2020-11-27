07e6f171f5f5b002c794efb093db2544446c7b54
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-27 15:15:37 +0800

    [Parking][Victor] feat: Complete park one car only when only 1 capacity left in parking lot

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,13 +1,23 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ParkingLot {
     private final int capacity;
+    private final List<Car> parkingCar;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        this.parkingCar = new ArrayList<>();
     }
 
     public Ticket park(Car car) {
+        if (parkingCar.size() >= capacity) {
+            return null;
+        }
+        parkingCar.add(car);
         return new Ticket();
     }
 }
