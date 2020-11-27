ea36f32367c8daba601817fd8181b4d9746fd907
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-27 17:44:28 +0800

    [Parking-Story3][Victor] test: Add park multiple with multiple capacity test

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -3,11 +3,16 @@ package com.oocl.cultivation;
 import com.oocl.parkingLotException.NotEnoughPositionException;
 import com.oocl.parkingLotException.UnrecognizedTicketException;
 
+import java.util.List;
+
 public class ParkingBoy {
     private ParkingLot parkingLot;
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
+    }
+
+    public ParkingBoy(List<ParkingLot> parkingLots) {
     }
 
     public Ticket park(Car car) throws NotEnoughPositionException {
