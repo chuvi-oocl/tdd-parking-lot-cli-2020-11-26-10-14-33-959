e7ccf035bda383390544ff26ce9440abcadbeaa1
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-27 15:07:51 +0800

    [Parking][Victor] test: Add get parking ticket by parking car

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -17,5 +18,15 @@ class ParkingBoyTest {
         parkingBoy.park(car);
         //then
         verify(parkingLot, times(1)).park(car);
+    }
+    @Test
+    void should_return_a_parking_ticket_when_park_the_car_given_a_car_and_parking_lot(){
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        final Ticket ticket = parkingLot.park(car);
+        //then
+        assertNotNull(ticket);
     }
 }
