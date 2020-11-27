92ab46a81667376e3a6413b50a579f12dec99119
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-27 15:11:35 +0800

    [Parking][Victor] feat: Complete get parking ticket by parking cary - update capacity

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,6 +1,12 @@
 package com.oocl.cultivation;
 
 public class ParkingLot {
+    private final int capacity;
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
     public Ticket park(Car car) {
         return new Ticket();
     }
