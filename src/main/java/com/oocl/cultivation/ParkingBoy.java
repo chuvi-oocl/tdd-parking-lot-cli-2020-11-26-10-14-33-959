956782fcc0f6405a9c4b62b69637b3343f4ebc37
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-27 17:52:46 +0800

    [Parking-Story3][Victor] feat: Complete park multiple with multiple capacity test

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -7,16 +7,27 @@ import java.util.List;
 
 public class ParkingBoy {
     private ParkingLot parkingLot;
+    private List<ParkingLot> parkingLots;
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
 
     public ParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
     }
 
     public Ticket park(Car car) throws NotEnoughPositionException {
-        return this.parkingLot.park(car);
+        if(parkingLot != null){
+            return this.parkingLot.park(car);
+        }
+        for (ParkingLot parkingLot : this.parkingLots) {
+            try {
+                return parkingLot.park(car);
+            } catch (NotEnoughPositionException ignored) {
+            }
+        }
+        throw new NotEnoughPositionException("Not Enough Position");
     }
 
     public Car fetch(Ticket ticket) throws UnrecognizedTicketException {
