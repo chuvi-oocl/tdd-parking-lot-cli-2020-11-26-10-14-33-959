b29d9458f29daff54cefe2d7c472c22a970c57b5
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-29 00:56:25 +0800

    [Parking-Story6][Victor] feat: Complete call park function in parking boy when park car given manager have no free capacity, parking boy with free capacity

--- src/main/java/com/oocl/cultivation/ParkingManager.java
+++ src/main/java/com/oocl/cultivation/ParkingManager.java
@@ -1,9 +1,23 @@
 package com.oocl.cultivation;
 
+import com.oocl.parkingLotException.NotEnoughPositionException;
+
 import java.util.List;
 
-public class ParkingManager extends ParkingBoy{
+public class ParkingManager extends ParkingBoy {
+    private List<ParkingBoy> parkingBoys;
+
     public ParkingManager(List<ParkingLot> parkingLots, List<ParkingBoy> parkingBoys) {
         super(parkingLots);
+        this.parkingBoys = parkingBoys;
+    }
+
+    @Override
+    public Ticket park(Car car) throws NotEnoughPositionException{
+        try {
+            return super.park(car);
+        } catch (NotEnoughPositionException e) {
+            return parkingBoys.get(0).park(car);
+        }
     }
 }
