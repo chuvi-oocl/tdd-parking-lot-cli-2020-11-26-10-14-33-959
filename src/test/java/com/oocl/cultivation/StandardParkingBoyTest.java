e56c262e4607c3a7011a673f759d20f7b790ad68
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-28 11:17:08 +0800

    [Parking-Story3][Victor] test: Add multiple parking lots with not enough capacity

--- src/test/java/com/oocl/cultivation/StandardParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/StandardParkingBoyTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -28,5 +28,26 @@ class StandardParkingBoyTest {
         //then
         assertNotNull(ticket1);
         assertNotNull(ticket2);
+    }
+    @Test
+    void should_only_two_car_parked_when_park_3_cars_given_2_parking_lots_with_1_capacity_each() throws NotEnoughPositionException {
+        //given
+        List<ParkingLot> parkingLots= new ArrayList<>();
+        parkingLots.add(new ParkingLot(1));
+        parkingLots.add(new ParkingLot(1));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        //when
+        final Ticket ticket1 = parkingBoy.park(car1);
+        final Ticket ticket2 = parkingBoy.park(car2);
+        final NotEnoughPositionException notEnoughPositionException = assertThrows(NotEnoughPositionException.class,
+                () -> parkingBoy.park(car3)
+        );
+        //then
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+        assertEquals("Not Enough Position", notEnoughPositionException.getMessage());
     }
 }
