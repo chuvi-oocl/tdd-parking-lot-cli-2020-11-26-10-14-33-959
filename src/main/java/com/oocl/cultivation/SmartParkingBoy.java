ff4c93f64e6069491df22a45915008ffeb97d9a4
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-28 12:07:34 +0800

    [Parking-Story4][Victor] test: Add smart parking boy should park car given multiple parking lots and multiple capacity

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,12 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SmartParkingBoy {
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+    }
+
+    public Ticket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,31 @@
+package com.oocl.cultivation;
+
+import com.oocl.parkingLotException.NotEnoughPositionException;
+import com.oocl.parkingLotException.UnrecognizedTicketException;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+public class SmartParkingBoyTest {
+    @Test
+    void should_parking_boy_park_car_in_multiple_parking_lots_when_park_multiple_cars_given_multiple_parking_lots_multiple_capacity(){
+        //given
+        List<ParkingLot> parkingLots= new ArrayList<>();
+        parkingLots.add(new ParkingLot(1));
+        parkingLots.add(new ParkingLot(1));
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        //when
+        final Ticket ticket1 = smartParkingBoy.park(car1);
+        final Ticket ticket2 = smartParkingBoy.park(car2);
+        //then
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+    }
+}
