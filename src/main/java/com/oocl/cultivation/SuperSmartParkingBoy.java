45406819e359c7ceb1005e9c12bff57aba9a941e
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-28 13:14:32 +0800

    [Parking-Story5][Victor] test: Add return parking tickets when park multiple cars given multiple parking lots

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -0,0 +1,12 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SuperSmartParkingBoy {
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
+    }
+
+    public Ticket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -0,0 +1,29 @@
+package com.oocl.cultivation;
+
+import com.oocl.parkingLotException.NotEnoughPositionException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SuperSmartParkingBoyTest {
+
+    @Test
+    void should_parking_boy_park_car_in_multiple_parking_lots_when_park_multiple_cars_given_multiple_parking_lots_multiple_capacity() {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(1));
+        parkingLots.add(new ParkingLot(1));
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        //when
+        final Ticket ticket1 = superSmartParkingBoy.park(car1);
+        final Ticket ticket2 = superSmartParkingBoy.park(car2);
+        //then
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+    }
+}
