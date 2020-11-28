85f4aa100629bee79c23045b3bbbdf7ac69f9ec4
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-28 12:17:30 +0800

    [Parking-Story4][Victor] feat: Complete park multiple car given multiple parking lots and multiple capacity

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -2,11 +2,8 @@ package com.oocl.cultivation;
 
 import java.util.List;
 
-public class SmartParkingBoy {
+public class SmartParkingBoy extends ParkingBoy{
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
-    }
-
-    public Ticket park(Car car) {
-        return null;
+        super(parkingLots);
     }
 }
